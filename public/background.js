893fdd4ba569b93d057085cf97ca32a5a1ff992f
HaPi <maoxiaobo@foxmail.com>
2019-06-20 20:00:23 +0800

    手动开关

--- public/background.js
+++ public/background.js
@@ -1,27 +1,46 @@
 // background.js
+let proxyOption = []
 function background () {
   chrome.storage.sync.get(['proxyOption', 'isOpen'], function(opts) {
-    if (opts.isOpen && opts.proxyOption) proxy(opts.proxyOption)
+    if (opts.isOpen && opts.proxyOption) {
+      proxyOption = opts.proxyOption
+      proxy()
+    }
   })
 }
 
-function proxy (options) {
-  chrome.webRequest.onBeforeRequest.addListener(
-    function (details) {
-      const url = details.url;
-      for (let i = 0; i < options.length; i++) {
-        let opt = options[i]
-        if (!opt.api || !opt.target) continue
-        if (url.indexOf(opt.api) > -1) {
-          return { redirectUrl: opt.target + url.split(opt.api)[1] }
-        }
+function callback (details) {
+  let options = proxyOption
+  const url = details.url;
+  if (options) {
+    for (let i = 0; i < options.length; i++) {
+      let opt = options[i]
+      if (!opt.api || !opt.target) continue
+      if (url.indexOf(opt.api) > -1) {
+        return { redirectUrl: opt.target + url.split(opt.api)[1] }
       }
+    }
+  }
 
-      return {cancel: false}
-    },
+  return {cancel: false}
+}
+
+function proxy () {
+  chrome.webRequest.onBeforeRequest.addListener(
+    callback,
     {urls: ["<all_urls>"]},
     ["blocking"]
   )
+  // alert(chrome.webRequest.onBeforeRequest.hasListeners())
+}
+
+function stopProxy () {
+  chrome.webRequest.onBeforeRequest.removeListener(
+    callback,
+    {urls: ["<all_urls>"]},
+    ["blocking"]
+  )
+  // alert(chrome.webRequest.onBeforeRequest.hasListeners())
 }
 
 background()
--- src/popup/App/App.vue
+++ src/popup/App/App.vue
@@ -1,37 +1,38 @@
 <template>
   <div class="main_app">
-    <div class="row">
-      <i class="el-icon-sort"></i><span>{{state?'已开启代理':'已关闭代理'}}</span>
-      <el-switch
-        v-model="state">
-      </el-switch>
+    <div class="row" @click="toggleProxy">
+      <i :class="state ? 'el-icon-check' : 'el-icon-close'"></i><span>{{state?'已开启':'已关闭'}}</span>
     </div>
     <div @click="toOptions" class="row"><i class="el-icon-setting"></i> <span class="setting">设置</span></div>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'app',
   data () {
     return {
-      state: true
-    }
-  },
-  watch: {
-    state: function () {
-      this.toggleProxy()
+      state: false
     }
   },
   methods: {
     toggleProxy () {
+      this.state = !this.state
+      let that = this
       chrome.storage.sync.set({ isOpen: this.state }, function() {
-        chrome.extension.getBackgroundPage().background()
+        that.state ? chrome.extension.getBackgroundPage().background() : chrome.extension.getBackgroundPage().stopProxy()
       })
     },
     toOptions () {
       window.open('./options.html')
     }
+  },
+  created () {
+    let that = this
+    chrome.storage.sync.get(['isOpen'], function (opts) {
+      that.state = opts.isOpen
+    })
   }
 }
 </script>
